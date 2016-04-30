ebc2116998691cee5f8a82edea912db344dc31ec
iAugus <iAugux@gmail.com>
2016-04-30 13:48:14 +0800

    Xcode Injection

---
+++ Phonetic/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Zoom Contacts
+//
+//  Created by Augus on 4/30/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// From here to end of file added by Injection Plugin //
+
+#ifdef DEBUG
+static char _inMainFilePath[] = __FILE__;
+static const char *_inIPAddresses[] = {"192.168.1.178", "127.0.0.1", 0};
+
+#define INJECTION_ENABLED
+#import "/tmp/injectionforxcode/BundleInjection.h"
+#endif
